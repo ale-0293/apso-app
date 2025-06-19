9c40f35ef84fb1804a56858a7ddfa12857b469df
ale-0293 <martin.a.ac@outlook.com>
2025-06-18 21:13:51 -0600

    LocalDateTime ZoneId.

--- src/main/java/com/apso/app/service/SorteoGrupalService.java
+++ src/main/java/com/apso/app/service/SorteoGrupalService.java
@@ -6,6 +6,7 @@ import com.apso.app.model.Usuario;
 import com.apso.app.repository.SorteoGrupalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.time.ZoneId;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,7 +20,7 @@ public class SorteoGrupalService {
     public SorteoGrupal guardarSorteo(String titulo, List<Estudiante> estudiantes, int numeroGrupos, String resultado, Usuario usuario) {
         SorteoGrupal sorteo = new SorteoGrupal();
         sorteo.setTitulo(titulo);
-        sorteo.setFechaHora(LocalDateTime.now());
+        sorteo.setFechaHora(LocalDateTime.now(ZoneId.of("America/El_Salvador")));
         sorteo.setCantidadGrupos(numeroGrupos);
         sorteo.setResultado(resultado);
         sorteo.setEstudiantes(estudiantes);
